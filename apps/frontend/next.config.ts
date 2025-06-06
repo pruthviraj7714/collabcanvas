f2c606e4d420f98b34c2fc4c0aafadb6abfcec52
pruthviraj <marganepruthviraj@gmail.com>
2025-06-06 23:26:10 +0530

    config fixes

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,20 +1,28 @@
 import type { NextConfig } from "next";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images : {
+  images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
       },
     ],
-  }
+  },
 };
 
 export default nextConfig;
