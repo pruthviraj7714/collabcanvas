c1f9ff9059e23ebf3c6116a7a198c654264bce96
pruthviraj <marganepruthviraj@gmail.com>
2025-06-21 22:33:48 +0530

    config fixes

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-
-    return config;
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
